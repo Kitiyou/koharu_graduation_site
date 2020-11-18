b88580c436c61a61f4771be448d7dad6960d50bb
kitiyou <912524139@qq.com>
2020-11-18 19:09:19 +0800

    添加从左右飞入的动画

--- js/index.js
+++ js/index.js
@@ -18,13 +18,41 @@ var modalClose = document.getElementsByClassName("modal-close")[0];
 var modalImgBox = document.getElementsByClassName("modal-img-box")[0];
 modalClose.onclick = closeModal;
 modalImgBox.onclick = closeModal;
+var lis = new Array();
 
 for (var i = 0; i < eventList.length; i++) {
     var children = eventList[i].children;
     for (var j = 0; j < children.length; j++) {
+        lis.push(children[j])
+
         var title = children[j].getElementsByClassName("event-title")[0].innerHTML;
         var imgNode = children[j].getElementsByClassName("event-img")[0];
         var src = imgNode.src.replace("/small/", "/images/");
-        imgNode.onclick = openModal(title, src)
+        imgNode.onclick = openModal(title, src);
     }
 }
+
+var liIndex = 0;
+var checkingFlag = false;
+function liEnter(li) {
+    li.classList.add("scrolled");
+}
+function liCheck() {
+    if (!checkingFlag && liIndex < lis.length) {
+        var clientHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        var boundingClientRect = lis[liIndex].getBoundingClientRect();
+        if (boundingClientRect.top + boundingClientRect.height / 2 < clientHeight) {
+            liEnter(lis[liIndex]);
+            liIndex++;
+            checkingFlag = true;
+            setTimeout(timeout, 150);
+        }
+    }
+}
+function timeout() {
+    checkingFlag = false;
+    liCheck();
+}
+window.onscroll = liCheck;
+window.onresize = liCheck;
+window.onload = liCheck;
