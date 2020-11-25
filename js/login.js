34f0048962777c25cdaa524fa681b0acd2a83199
kitiyou <912524139@qq.com>
2020-11-25 20:06:04 +0800

    做了一半登陆动画

--- js/login.js
+++ js/login.js
@@ -1,11 +1,20 @@
+function loginBtnClick() {
+    document.getElementsByClassName("login-btn")[0].classList.add("finish");
+    document.getElementsByClassName("input-div")[0].classList.remove("invisible");
+}
+var loginFlag = false;
+var passwordInput = document.getElementsByClassName("pwd-input")[0];
 function loginSubmit() {
-    var password = document.getElementById("password").value;
-    if (isCorrectPassword(password)) {
-        window.sessionStorage.setItem("khr_password", password);
-        return true;
+    if (!loginFlag && isCorrectPassword(passwordInput.value)) {
+        passwordInput.disabled = "disabled"
+        window.sessionStorage.setItem("khr_password", passwordInput.value);
+        loginFlag = true;
+
+        document.getElementById("rotation-group").classList.add("rotation");
+        document.getElementsByClassName("input-div")[0].classList.add("finish");
+        document.getElementsByClassName("input-rect")[0].classList.add("finish");
+        document.getElementsByClassName("input-rect")[1].classList.add("finish");
+        document.getElementsByClassName("success-div")[0].classList.remove("invisible");
     }
-    else {
-        window.alert("パスワードが正しくありません。")
-        return false;
-    }
+    console.log(loginFlag);
 }
